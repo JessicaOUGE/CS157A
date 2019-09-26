c5d0a11ee71c78166df0b68bb7aabe8e1c2e5f61
Ge Ou <ougejiajia@gmail.com>
2019-09-26 11:40:07 -0700

    nodejs express and mongodb

---
+++ nodetest2/models/employee.js
@@ -0,0 +1,21 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Employee = sequelize.define('Employee', {
+    name: {
+      type:DataTypes.STRING,
+      allowNull: false,
+  }, 
+    designation: {
+        type: DataTypes.STRING,
+        defaultValue:false,
+    },
+});
+  Employee.associate = function(models) {
+    // associations can be defined here
+    Employee.belongsTo(models.Company,{
+      foreignKey: 'companyId',
+      as: 'CASCADE',
+    });
+  };
+  return Employee;
+};
